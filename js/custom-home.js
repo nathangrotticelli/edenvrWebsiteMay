b48b936d2ea9026e4202d00cdbbe8bb12027b5a0
Nathan Grotticelli <nathangrotticelli@gmail.com>
2017-12-06 18:15:57 -0500

    staging home js 13

--- js/custom-home.js
+++ js/custom-home.js
@@ -33,7 +33,7 @@ function onVrViewLoad() {
 window.addEventListener('load', onVrViewLoad2)
 function onVrViewLoad2() {
   var vrView2 = new VRView.Player('#vrview', {
-    image: 'http://s3.us-east-2.amazonaws.com/eden-pics/HollwoodShot1.jpg',
+    image: 'images/eden-pics/HollwoodShot1.jpg',
     is_stereo: true
   });
 }

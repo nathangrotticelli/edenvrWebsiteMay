a2f2e5550ae80bc6f3053983328225d0113d4f5a
Nathan Grotticelli <nathangrotticelli@gmail.com>
2017-12-06 18:07:01 -0500

    staging home js 11

--- js/custom-home.js
+++ js/custom-home.js
@@ -22,14 +22,14 @@
             'opacity': 1 - (windowScroll / 300)
         });
     });
-
-var onVrViewLoad = function () {
+window.addEventListener('load', onVrViewLoad)
+function onVrViewLoad() {
   var vrView = new VRView.Player('#vrview', {
-    image: 'http://s3.us-east-2.amazonaws.com/eden-pics/HollwoodShot1.jpg',
+    video: 'link/to/video.mp4',
     is_stereo: true
   });
-};
-	window.addEventListener('load', onVrViewLoad);
+}
+	
 
 
 

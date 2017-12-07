1629d621ddfc9798ca8d3aaede57f700091b77ee
Nathan Grotticelli <nathangrotticelli@gmail.com>
2017-12-06 19:11:15 -0500

    staging home js 23

--- js/custom-home.js
+++ js/custom-home.js
@@ -88,18 +88,10 @@
 		}
 function onVrViewLoad() {
   var vrView = new VRView.Player('#vrview', {
-    image: '//http://s3.us-east-2.amazonaws.com/eden-pics/HollwoodShot1.jpg',
+    image: '//storage.googleapis.com/vrview/index.html?image=//s3.us-east-2.amazonaws.com/eden-pics/HollwoodShot1.jpg',
     is_stereo: true
   });
 }
-
-function onVrViewLoad2() {
-  var vrView2 = new VRView.Player('#vrview2', {
-    image: '//storage.googleapis.com/vrview/index.html?image=//s3.us-east-2.amazonaws.com/eden-pics/HollwoodShot1.jpg',
-	is_stereo: true
-  });
-}
-
 
 		//TaurusMenu
 

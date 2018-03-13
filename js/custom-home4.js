22157817a95f8b0d96cccc1fc23fc0f0a9bdf209
Nathan Grotticelli <nathangrotticelli@gmail.com>
2018-03-12 23:09:56 -0700

    mobile and loader fix 1.7

--- js/custom-home4.js
+++ js/custom-home4.js
@@ -2,13 +2,6 @@
 
 
 	$(document).ready(function() {
-
-		Royal_Preloader.config({
-			mode           : 'logo',
-			background     : 'black',
-			showProgress   : false,
-			showPercentage : false
-		});
 			/* slider revolution */
 
 					// var tpj=jQuery;
@@ -218,39 +211,39 @@ window.setTimeout(offsetAnchor, 0);
 	});
 
 		/* DebouncedResize Function */
-		(function ($) {
-			var $event = $.event,
-				$special,
-				resizeTimeout;
+		// (function ($) {
+		// 	var $event = $.event,
+		// 		$special,
+		// 		resizeTimeout;
 
 
-			$special = $event.special.debouncedresize = {
-				setup : function () {
-					$(this).on('resize', $special.handler);
-				},
-				teardown : function () {
-					$(this).off('resize', $special.handler);
-				},
-				handler : function (event, execAsap) {
-					var context = this,
-						args = arguments,
-						dispatch = function () {
-							event.type = 'debouncedresize';
+		// 	$special = $event.special.debouncedresize = {
+		// 		setup : function () {
+		// 			$(this).on('resize', $special.handler);
+		// 		},
+		// 		teardown : function () {
+		// 			$(this).off('resize', $special.handler);
+		// 		},
+		// 		handler : function (event, execAsap) {
+		// 			var context = this,
+		// 				args = arguments,
+		// 				dispatch = function () {
+		// 					event.type = 'debouncedresize';
 
-							$event.dispatch.apply(context, args);
-						};
-
-
-					if (resizeTimeout) {
-						clearTimeout(resizeTimeout);
-					}
+		// 					$event.dispatch.apply(context, args);
+		// 				};
 
 
-					execAsap ? dispatch() : resizeTimeout = setTimeout(dispatch, $special.threshold);
-				},
-				threshold : 150
-			};
-		} )(jQuery);
+		// 			if (resizeTimeout) {
+		// 				clearTimeout(resizeTimeout);
+		// 			}
+
+
+		// 			execAsap ? dispatch() : resizeTimeout = setTimeout(dispatch, $special.threshold);
+		// 		},
+		// 		threshold : 150
+		// 	};
+		// } )(jQuery);
 
 
 })(jQuery);
